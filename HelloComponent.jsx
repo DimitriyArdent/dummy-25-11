7535b06d4676b343e308346b190bb6b172a5ed82
DimitriyArdent <109689205+DimitriyArdent@users.noreply.github.com>
2023-11-25 19:40:32 +0200

    my second commit

---
+++ HelloComponent.jsx
@@ -0,0 +1,28 @@
+// HelloComponent.js
+// HelloComponent.js
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const HelloComponent2 = () => {
+  const [message, setMessage] = useState('');
+
+  const fetchData = () => {
+    // Send a request to the server when the button is clicked
+    axios.get('http://localhost:3001/hello')
+      .then(response => setMessage(response.data))
+      .catch(error => console.error('Error:', error));
+  };
+
+  return (
+    <div>
+  
+
+      <h1>Hello Component   </h1>
+      <div>Hera are some changes</div>
+      <button onClick={fetchData}>Fetch Message</button>
+      <p>Message from the server: {message}</p>
+    </div>
+  );
+};
+
+export default HelloComponent2;
